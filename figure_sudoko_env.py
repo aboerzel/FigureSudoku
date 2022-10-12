12a774e053597a4d17b2faa9daed92c58bc05f4d
Andreas <andreas.boerzel@gmx.de>
2022-10-12 20:48:28 +0200

    SAC Agent

--- figure_sudoko_env.py
+++ figure_sudoko_env.py
@@ -1,14 +1,13 @@
-from enum import Enum
-
+import itertools
 import numpy as np
-
+from enum import Enum
 from shapes import Geometry, Color
 from sudoku_generator import SudokuGenerator
 
 
 class Reward(Enum):
     FORBIDDEN = -10
-    CONTINUE = 0
+    CONTINUE = -1
     DONE = 10
 
 
@@ -20,10 +19,13 @@ class FigureSudokuEnv:
         self.gui = gui
         self.rows = len(self.geometries)
         self.cols = len(self.colors)
+        self.figures = np.array(list(itertools.product(self.geometries, self.colors)))
+        fields = np.array(list(itertools.product(np.arange(self.rows), np.arange(self.cols))))
+        self.actions = np.array(list(itertools.product(self.figures, fields)), dtype=object)
         self.state = np.array([x for x in [[(Geometry.EMPTY.value, Color.EMPTY.value)] * self.rows] * self.cols])
 
         self.state_size = len(self.state.flatten())
-        self.actions_dims = np.array([[0, self.rows - 1], [0, self.cols - 1], [Geometry.CIRCLE.value, Geometry.HEXAGON.value], [Color.RED.value, Color.YELLOW.value]])
+        self.action_size = len(self.actions)
 
         self.generator = SudokuGenerator(geometries, colors)
 
@@ -36,8 +38,42 @@ class FigureSudokuEnv:
             self.gui.display_state(self.state)
         return self.state.flatten()
 
+    def get_possible_actions(self, state):
+        state = state.reshape(16, 2)
+
+        # get used figures
+        used_figures = state[np.logical_and(state[:, 0] != Geometry.EMPTY.value, state[:, 1] != Color.EMPTY.value)]
+        used_figures = [[Geometry(f[0]), Color(f[1])] for f in used_figures]
+
+        # get used cells
+        test = np.array([a for a in np.where(np.logical_and(state[:, 0] == Geometry.EMPTY.value, state[:, 1] == Color.EMPTY.value))]).squeeze(axis=0)
+
+        used_cells = []
+        for x in test:
+            row = int(x / self.rows)
+            col = x % self.cols
+            used_cells.append([row, col])
+
+        possible_actions = self.actions.copy()
+        # filter out used figures
+        possible_actions = [a for a in possible_actions if
+                            len([f for f in used_figures if a[0][0] == f[0] and a[0][1] == f[1]]) == 0]
+        # filter out used cells
+        possible_actions = [a for a in possible_actions if
+                            len([c for c in used_cells if a[1][0] == c[0] and a[1][1] == c[1]]) != 0]
+
+        # get indices
+        possible_actions_ind = np.where([True if len([a for a in possible_actions if
+                                                      a[0][0] == b[0][0] and a[0][1] == b[0][1] and a[1][0] == b[1][0]
+                                                        and a[1][1] == b[1][1]]) > 0 else False for b in self.actions])[0]
+
+        return possible_actions_ind
+
     def step(self, action):
-        row, col, geometry, color = action
+        target_action = self.actions[action]
+
+        (geometry, color) = target_action[0]
+        (row, col) = target_action[1]
 
         temp_state = [geometry.value, color.value]
 
--- train.py
+++ train.py
@@ -3,9 +3,9 @@ from collections import deque
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
+from Discrete_SAC_Agent import SACAgent
 from figure_sudoko_env import FigureSudokuEnv
 from hyperparameters import *
-from ddpg_agent import DDPGAgent
 from shapes import Geometry, Color
 
 
@@ -14,14 +14,10 @@ def train_sudoku(gui, stop):
     geometries = np.array([Geometry.CIRCLE, Geometry.QUADRAT, Geometry.TRIANGLE, Geometry.HEXAGON])
     colors = np.array([Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW])
     env = FigureSudokuEnv(geometries, colors, gui=gui)
-    state_size = env.state_size
-    action_dims = env.actions_dims
 
-    agent = DDPGAgent(state_size=state_size, action_dims=action_dims, batch_size=BATCH_SIZE, buffer_capacity=BUFFER_SIZE,
-                      ou_noise=np.array(OU_NOISE), ou_theta=OU_THETA,
-                      actor_lr=ACTOR_LR, critic_lr=CRITIC_LR, gamma=GAMMA, tau=TAU)
+    agent = SACAgent(env)
 
-    agent.load_model_weights(OUTPUT_DIR)
+    #agent.load_model_weights(OUTPUT_DIR)
 
     # hyperparameter
     start_episode = 1
@@ -41,15 +37,25 @@ def train_sudoku(gui, stop):
         if stop():
             break
 
+        evaluation_episode = episode % TRAINING_EVALUATION_RATIO == 0
+
         state = env.reset(level=level)  # reset the environment
         episode_score = 0
         for timestep in range(1, MAX_STEPS_PER_EPISODE + 1):
-            action = agent.act(state, train=True)
-            print(f'Episode {episode:08d} - Step {timestep:04d}\tAction: {action[0]} {action[1]} {action[2]} {action[3]}', end='\r')
-            next_state, reward, done = env.step(np.array([action[0], action[1], Geometry(action[2]), Color(action[3])]))
-            agent.step(state, action, reward, next_state, done)
+            action = agent.get_next_action(state, evaluation_episode=evaluation_episode)
+            print(f'Episode {episode:08d} - Step {timestep:04d}\tAction: {action}', end='\r')
+            next_state, reward, done = env.step(action)
+
+            if not evaluation_episode:
+                agent.train_on_transition(state, action, next_state, reward, done)
+            else:
+                episode_score += reward
+
+            #agent.train_on_transition(state, action, next_state, reward, done)
+            #episode_score += reward
+
             state = next_state
-            episode_score += reward
+
             if done:
                 print(f'Episode {episode:08d} - Step {timestep:04d}\tEpisode Score: {episode_score:.2f}\tdone!')
                 break
@@ -63,21 +69,21 @@ def train_sudoku(gui, stop):
 
         if episode % 10 == 0:
             print(f'\rEpisode {episode:08d}\tAverage Score: {avg_score:.2f}')
-            agent.save_model_weights(OUTPUT_DIR)
+            agent.save(MODEL_FILE)
 
         # print(f'Episode {episode:06d}\tAvg Score: {avg_score:.2f}\tBest Avg Score: {best_avg_score:.2f}')
 
         # save best weights
         if episode > warmup_episodes and avg_score > best_avg_score:
             best_avg_score = avg_score
-            agent.save_model_weights(OUTPUT_DIR)
+            agent.save(MODEL_FILE)
             print(f'Episode {episode:08d}\tWeights saved!\tBest Avg Score: {best_avg_score:.2f}')
 
         # stop training if target score was reached
         if episode > warmup_episodes and avg_score >= TARGET_SCORE:
-            agent.save_model_weights(OUTPUT_DIR)
+            agent.save(MODEL_FILE)
             print(f'\nEnvironment solved in {episode} episodes!\tAverage Score: {avg_score:.2f}')
             break
 
-    agent.save_model_weights(OUTPUT_DIR)
+    agent.save(MODEL_FILE)
     print(f'Training finished!\tBest Avg Score: {best_avg_score:.2f}')
